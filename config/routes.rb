fb33e63d5a7acfcfe0b63948cc6eedf7e4e0c043
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-08-16 10:46:11 +0000

    [Add]4-25

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/about'
 
   devise_for :users
   root to: "homes#top"
-  get "homes/about" => "homes#about", as: "about"
+  get "/homes/about" => "homes#about", as: "about"
 
-  resources :books, onle: [:create, :edit, :index, :show, :destroy]
+  resources :books, only: [:create, :edit, :index, :show, :destroy]
   resources :users, only: [:edit, :index, :show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
