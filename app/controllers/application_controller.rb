a41b6be2bbc2fa5e6ff98e365f104e68614f94dc
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-08-18 09:01:45 +0000

    [Add]4-25

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
  before_action :configure_permitted_parameters, if: :devise_controller?
  prepend_before_action :require_no_authentication, only: [:new, :create, :cancel]
 
+ def after_sign_in_path_for(resource)
+  user_path(current_user)
+ end
+
+ def after_sign_out_path_for(resource)
+  root_path
+ end
+
  protected
 
  def configure_permitted_parameters
