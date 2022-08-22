54813cd04d69d6101aa35e9f050cb57f3c0f9a2b
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-08-22 12:49:42 +0000

    [Add]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,6 @@ class BooksController < ApplicationController
   def index
    @book = Book.new
    @books =Book.all
-   @user = current_user
   end
 
   def create
@@ -11,15 +10,16 @@ class BooksController < ApplicationController
    @book.user_id = current_user.id
 
    if @book.save
-    redirect_to book_path(@book.user_id)
+    redirect_to book_path(@book.id)
    else
+    @books = Book.all
     render :index
    end
   end
 
   def show
-   @user = current_user
-   @book = Book.new
+   @newbook = Book.new
+   @book = Book.find(params[:id])
   end
 
   def edit
