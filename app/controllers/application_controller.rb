f6e8fec381a56f36dfe7ef01bcdd65c81066f8e1
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-08-23 10:48:21 +0000

    [Add]4-25

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
+ before_action :authenticate_user!, except: [:top, :about]
+
  before_action :configure_permitted_parameters, if: :devise_controller?
  prepend_before_action :require_no_authentication, only: [:new, :create, :cancel]
- add_flash_types :success, :info, :warning, :danger
 
  def after_sign_in_path_for(resource)
   user_path(current_user)
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
   def create
    @book = Book.new(book_params)
    @book.user_id = current_user.id
-
+   @user = current_user
    if @book.save
     redirect_to book_path(@book.id), notice: 'You have created book successfully.'
    else
