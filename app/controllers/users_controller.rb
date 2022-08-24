42973323d58627746e92efc313bf9158a37c06a3
mikitakaya <108921094mikitakaya@users.noreply.github.com>
2022-08-24 05:08:00 +0000

    [Add]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+ before_action :correct_user, only: [:edit, :update]
+
   def index
    @user = current_user
    @book = Book.new
@@ -27,7 +29,12 @@ class UsersController < ApplicationController
   private
 
   def user_params_update
-   params.require(:user).permit(:name, :image, :introduction)
+   params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
+  def correct_user
+   @user = User.find(params[:id])
+   redirect_to user_path(current_user) unless @user == current_user
   end
 
 end
